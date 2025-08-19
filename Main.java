d5adbc3e4693d1a67fbb12b2d934738776a3f569
ArTyy022 <toledoarthur11@gmail.com>
2025-08-19 13:42:04 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import Personagem.Arqueiro;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+        Arqueiro arqueiro = new Arqueiro();
+        arqueiro.nome = "Cister";
+        arqueiro.idade = 47;
+        arqueiro.exibir();
+        }
+    }
